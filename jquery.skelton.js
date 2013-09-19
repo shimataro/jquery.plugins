51c9567435807069f50de7bc86223438e029fc88
shimataro <shimataro@zelkova.cc>
2013-09-19 23:06:27 +0900

    Addition: * skelton files

---
+++ jquery.skelton.js
@@ -0,0 +1,22 @@
+/*!
+ * Skelton of jQuery plugin
+ * requires:
+ *  jQuery x.x.x+
+ *  other libraries...
+ */
+(function($, window)
+{
+	$.fn.extend({
+		skelton: (function($)
+		{
+			/**
+			 * description
+			 * @function
+			 */
+			return function()
+			{
+				return this;
+			};
+		})($)
+	});
+})(jQuery, window);
